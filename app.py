e62cee510fa3960e56903eeca7280b043e0712d3
Abhishek Kommu <kommuabhishek84@gmail.com>
2026-08-05 10:14:10 +0000

    html app.py updated

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,jsonify,request
 
 app = Flask(__name__)
 
+users_db = {}
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -22,7 +24,7 @@ def courses():
 def login():
     return render_template("login.html")     
 
-@app.route("/register",method=["POST,GET"])
+@app.route("/register",methods=["POST","GET"])
 def register():
     if request.method=="POST":
         name=request.form["name"]
@@ -33,9 +35,31 @@ def register():
         course=request.form["course"]
     return render_template("register.html")   
 
-@app.route("/trainers")
-def trainers():
-    return render_template("trainers.html")             
+@app.route('login',methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        return render_template("login.html")
+    return render_template("login.html")
+
+@app.route('/api/register', methods=["POST"])
+def api_register():
+    data = request.get_json()
+    email = data.get('email')
+    if email in users_db:
+        return jsonify({"status": "error", "message": "User already exists with this email"}), 400
+    users_db[email] = data 
+    return jsonify({"status": "success", "message": "User Registration successfully"})  
+
+@app.route('/api/login', methods=["POST"])
+def api_login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    user = users_db.get(email)
+    if user and user.get('passward') == password:
+        return jsonify({"status": "success", "message": "Login successful! welcome back,"})
+    else:
+        return jsonify({"status": "error", "message": "Invalid email or password"}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)    
